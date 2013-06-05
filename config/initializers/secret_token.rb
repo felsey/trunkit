5582cfd464adbfa541638414e92e93d876d68f4d
Sam Felsenthal <sfelsey@gmail.com>
2013-06-05 01:13:33 -0600

    Edit the File

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trunkit::Application.config.secret_token = '1d55896f1bc721423f2747461bbc6de3461bfb569f756247cf4d61a73dfc4a95a5d2776b5eba99e7b250ba2100088c924ade437c1e7f71b8b1228a7deeb2a682'
